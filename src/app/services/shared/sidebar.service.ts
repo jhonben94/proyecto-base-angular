3c36998d5de3098370d5d6b7cdbd8e4be98d0ca9
Jhony Alfredo Benitez Martinez <jhonyben.94@gmail.com>
2020-03-08 13:22:35 -0300

    SideBar
    Se modifica el sidebar para que muestre dinamicamente los iconos ya sea por sub-modulo o por item de sub-modulo

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -9,31 +9,61 @@ export class SidebarService {
     this.menu =[
       {
         modulo:'Parametricos',
-        iconoModulo:'',
+        iconoSubModulo:'address-book',
         moduloItems: [
           {
               id:'cliente',
               subModulo : 'Clientes',
+              iconoSubModulo: 'fas fa-user',
               subItems: [
                 {
                   url:'',
-                  icono:'address-card',
+                  icono:'fas fa-user',
                   titulo:'Agregar Cliente'
                 },
                 {
                   url:'',
-                  icono:'address-card',
+                  icono:'fas fa-address-card',
                   titulo:'Modificar Cliente'
                 },
                 {
                   url:'',
-                  icono:'address-card',
+                  icono:'fas fa-user-friends',
                   titulo:'Listar Clientes'
+                }
+              ]
+          },
+        ]
+      },
+   
+      {
+        modulo:'Servicios',
+        moduloItems: [
+          {
+              id:'submodulo2',
+              subModulo : 'SubModulo2',
+              iconoSubModulo:'fas fa-ad',
+              subItems: [
+                {
+                  url:'',
+                  icono:'fas fa-ad',
+                  titulo:'Tipo Producto'
+                },
+                {
+                  url:'',
+                  icono:'fas fa-alipay',
+                  titulo:'Tipo Documento'
+                },
+                {
+                  url:'',
+                  icono:'fas fa-angle-double-left',
+                  titulo:'Categorias'
                 }
               ]
           },
           {
             id:'submodulo3',
+            iconoSubModulo:'fas fa-adjust',
             subModulo : 'SubModulo3',
             subItems: [
               {
@@ -56,56 +86,24 @@ export class SidebarService {
 
         ]
       },
-   
       {
-        modulo:'Servicios',
-        iconoModulo:'',
+        modulo:'modulon',
         moduloItems: [
           {
-              id:'submodulo2',
-              subModulo : 'SubModulo2',
+              id:'modulon',
+              iconoSubModulo:'fas fa-align-center',
+              subModulo : 'Modulo N',
               subItems: [
                 {
                   url:'',
                   icono:'address-card',
-                  titulo:'Tipo Producto'
-                },
-                {
-                  url:'',
-                  icono:'address-card',
-                  titulo:'Tipo Documento'
-                },
-                {
-                  url:'',
-                  icono:'address-card',
-                  titulo:'Categorias'
+                  titulo:'Agregar Cliente'
                 }
               ]
-          },
-          {
-            id:'submodulo3',
-            subModulo : 'SubModulo3',
-            subItems: [
-              {
-                url:'',
-                icono:'address-card',
-                titulo:'Tipo Producto'
-              },
-              {
-                url:'',
-                icono:'address-card',
-                titulo:'Tipo Documento'
-              },
-              {
-                url:'',
-                icono:'address-card',
-                titulo:'Categorias'
-              }
-            ]
           }
 
         ]
-      }
+      },
     ];
    }
    obtenerSideBar(){
